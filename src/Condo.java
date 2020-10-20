2bbfb204431dd3893d6a530e2a4499e477111376
Jonathan Zollinger <jonathan.zollinger@outlook.com>
2020-10-20 00:30:00 -0600

    adds Condo class, a subclass of House

---
+++ src/Condo.java
@@ -0,0 +1,17 @@
+public class Condo extends Residential{
+    //values
+    int floorLvl;
+
+    //constructors
+    public Condo(){
+        floorLvl = 0;
+    }
+    //methods
+    public int getFloorLvl(){
+        return floorLvl;
+    }
+    public void setFloorLvl(int floorLvl){
+        this.floorLvl = floorLvl;
+    }
+
+}//end public class Condo extends Residential
