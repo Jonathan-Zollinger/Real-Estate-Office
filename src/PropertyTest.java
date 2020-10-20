e1e9266c97fdc6d2e60d7d8081d0a923f3177d4d
Jonathan Zollinger <jonathan.zollinger@outlook.com>
2020-10-20 00:31:40 -0600

    creates tests for all classes and methods thus far

---
+++ src/PropertyTest.java
@@ -0,0 +1,18 @@
+class PropertyTest {
+
+    @org.junit.jupiter.api.Test
+    void getStreetAddress() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getZip() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setStreetAddress() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setZip() {
+    }
+}
---
+++ src/ResidentialTest.java
@@ -0,0 +1,28 @@
+import org.junit.jupiter.api.Test;
+
+class ResidentialTest {
+
+    @Test
+    void getBeds() {
+    }
+
+    @Test
+    void getBaths() {
+    }
+
+    @Test
+    void getSize() {
+    }
+
+    @Test
+    void setBeds() {
+    }
+
+    @Test
+    void setBaths() {
+    }
+
+    @Test
+    void setSize() {
+    }
+}
