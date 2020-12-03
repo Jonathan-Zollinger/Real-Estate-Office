ee8312eeeca706ae31043aa52c7726c693d20659
Jonathan Zollinger <jonathan.zollinger@outlook.com>
2020-12-02 18:47:27 -0700

    adds Listings.java class, fleshes out constructor and methods

---
+++ src/Listings.java
@@ -0,0 +1,44 @@
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Set;
+
+public class Listings implements Listable{
+    //attributes
+    HashMap<String,Residential> hashMap;
+
+    Listings(){
+        hashMap = new HashMap<>();
+    }
+
+    //methods
+    @Override
+    public HashMap<String, Residential> getListings() {
+        return this.hashMap;
+    }
+
+    @Override
+    public Residential getListing(String address) {
+        return hashMap.get(address);
+    }
+
+    @Override
+    public Set<String> getStreetAddresses() {
+        return hashMap.keySet();
+    }
+
+    @Override
+    public Collection<Residential> getResidences() {
+        return hashMap.values();
+    }
+
+    @Override
+    public int getListingCount() {
+        return hashMap.size();
+    }
+
+    @Override
+    public void addListing(String address, Residential residential) {
+        hashMap.put(address,residential);
+        System.out.printf("added the residence at %s",address);
+    }
+}
